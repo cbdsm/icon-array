c4202c60c7fed3aabd545740335e2caed76c902f
Mark Dickson <mark@sitesteaders.com>
2012-05-01 13:38:56 -0400

    images are both borked in different ways...awesome

--- db/schema.rb
+++ db/schema.rb
@@ -11,24 +11,26 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120420162509) do
+ActiveRecord::Schema.define(:version => 20120501170450) do
 
   create_table "pictographs", :force => true do |t|
     t.string   "title"
-    t.integer  "cell_width",      :default => 25
-    t.integer  "cell_height",     :default => 45
-    t.integer  "cell_spacing",    :default => 5
-    t.integer  "cols",            :default => 10
-    t.integer  "rows",            :default => 10
-    t.boolean  "axis_labels",     :default => true
-    t.string   "axis_position",   :default => "left"
-    t.string   "axis_font",       :default => "Arial"
-    t.integer  "axis_font_size",  :default => 12
-    t.datetime "created_at",                               :null => false
-    t.datetime "updated_at",                               :null => false
-    t.string   "title_font",      :default => "Helvetica"
-    t.integer  "title_size",      :default => 18
-    t.string   "title_alignment", :default => "left"
+    t.integer  "cell_width",       :default => 25
+    t.integer  "cell_height",      :default => 45
+    t.integer  "cell_spacing",     :default => 5
+    t.integer  "cols",             :default => 10
+    t.integer  "rows",             :default => 10
+    t.boolean  "axis_labels",      :default => true
+    t.string   "axis_position",    :default => "left"
+    t.string   "axis_font",        :default => "Arial"
+    t.integer  "axis_font_size",   :default => 12
+    t.datetime "created_at",                                :null => false
+    t.datetime "updated_at",                                :null => false
+    t.string   "title_font",       :default => "Helvetica"
+    t.integer  "title_font_size",  :default => 18
+    t.string   "title_alignment",  :default => "left"
+    t.string   "legend_font",      :default => "Arial"
+    t.integer  "legend_font_size", :default => 12
   end
 
   create_table "risks", :force => true do |t|
