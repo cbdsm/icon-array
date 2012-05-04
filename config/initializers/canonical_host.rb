2144bbaf57aa8499c679c7f8b83897a33e8a94f3
Mark Dickson <mark@sitesteaders.com>
2012-05-04 17:32:52 -0400

    minor tweak

--- config/initializers/canonical_host.rb
+++ config/initializers/canonical_host.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use Rack::CanonicalHost do
   case ENV['RACK_ENV'].to_sym
-    # when :development then 'lvh.me:3000' #make sure this is in your dns or /etc/hosts file
+    # when :development then 'localhost:3000' #make sure this is in your dns or /etc/hosts file
     when :staging then 'staging.iconarray.com'
     when :production then 'www.iconarray.com'
   end
