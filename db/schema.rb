9a0f6b9965373208557f6f38d15a90ef6e22eed0
Mark Dickson <mark@sitesteaders.com>
2012-10-01 17:49:07 -0400

    legends now default to show

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120510173829) do
+ActiveRecord::Schema.define(:version => 20121001214711) do
 
   create_table "pictographs", :force => true do |t|
     t.string   "title"
