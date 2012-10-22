a636ddee1b5ea9e683f56ea0faed2faf11e0d620
Mark Dickson <mark@sitesteaders.com>
2012-10-22 11:15:23 -0400

    fix for IE FF image submit issue

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -35,5 +35,15 @@ module ApplicationHelper
     rgb << hex[4..5].to_i(16)
     return rgb
   end
+
+  # NOTE: this would be awesome, except for the width/height specs
+  def html5_image_submit_tag(image, options={})
+    style = options[:style] || ''
+    style += "background-image:url(assets/#{image}); display: block; width: 59px; height: 59px; text-indent: -1999px; overflow: hidden; background-color:white; border: none;"
+    options[:style] = style
+    value = options[:value]
+    options.delete(:value)
+    submit_tag(value, options)
+  end
   
 end
