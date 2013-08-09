89a0ee8affe9de50d28c0060ec8ec439914f70af
Mark Dickson <mark@sitesteaders.com>
2013-08-09 11:52:04 -0400

    fix for assets precompile

--- config/application.rb
+++ config/application.rb
@@ -55,5 +55,8 @@ module IconArray
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Requiring an ENV borks Heroku apps, so we disable that
+    config.assets.initialize_on_precompile = false
   end
 end
